f9b537ebca1a4f0cad1b0423eccb3ae530d81ec6
trilltown <phil.awich@gmail.com>
2019-12-23 00:03:02 -0500

    Add SVG images

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMObjectLoaded", () => {
     // Add a button which toggles the background color
-    // Also changes the font and links on click
+    // Also changes the font and links on click 
 })
